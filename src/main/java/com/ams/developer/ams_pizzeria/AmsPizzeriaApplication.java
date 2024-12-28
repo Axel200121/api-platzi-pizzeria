1db9be32e779a8b5b33ededd807ca4d0e4d9061e
Axel200121 <axelmartinez212001@gmail.com>
2024-12-28 10:58:07 -0600

    first commit

---
+++ src/main/java/com/ams/developer/ams_pizzeria/AmsPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package com.ams.developer.ams_pizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ams/developer/ams_pizzeria/AmsPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ams.developer.ams_pizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmsPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
